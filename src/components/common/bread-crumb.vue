354b73b0ec96dc85f87efdd18a9307c64605417d
ChenXiaoFeng <1287761424@qq.com>
2020-03-05 18:50:55 +0800

    封装面包屑组件

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- 使用面包屑组件  separator=">"这是选项与选项之间的分隔符-->
+  <el-breadcrumb separator=">">
+    <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+    <!-- 第二个选项是动态的 需要以传值的方式传进来 props 传值 slot插槽 -->
+    <el-breadcrumb-item>
+      <!-- 内容是动态的 -->
+      <!-- 定义一个具名插槽 -->
+        <slot name='title'></slot>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
